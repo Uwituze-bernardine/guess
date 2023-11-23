7491f6a07085bda8419f3be38ddb323184b41103
Uwituze bernardine <bernardineuwituze6@gmail.com>
2023-11-23 15:58:16 +0200

    add guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,36 @@
+import random
+count = 0
+while count < 3:
+
+    def generate_random_number():
+        pass
+
+
+    number = 9
+
+
+    def take_user_guess():
+        pass
+
+
+    guess_number = input("enter guess number ")
+
+
+    def check_guess(random_number, user_guess):
+
+        if random_number == user_guess:
+            print("you win")
+        else:
+            print("you failed")
+
+
+    def main():
+        generate_random_number()
+        take_user_guess()
+        check_guess(number, guess_number)
+
+
+    if __name__ == "__main__":
+        main()
+    count = count + 1
+
